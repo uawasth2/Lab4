c27cb059a8c2163c9cbcf9cea56e32e0a37b0470
Navam <Navam@DESKTOP-4OVM6FR>
2017-09-26 23:30:18 -0500

    CheckStyle errors corrected

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,36 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * this is dumb.
+ */
+public class ReclamationProject {
+    /**
+     * do stuff.
+     * @param a unused
+     * @param b unused
+     * @return string
+     */
+    public static String doit(final String a, final String b) {
+        String newa = "";
+        String newb = "";
+        if (a.length() > b.length()) {
+            String c = a;
+            newa = b; newb = c;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < newa.length(); i++) {
+            for (int j = newa.length() - i; j > 0; j--) {
+                for (int k = 0; k < newb.length() - j; k++) {
+                    if (newa.regionMatches(i, newb, k, j) && j > r.length()) {
+                        newa.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
+        }
+        return r;
+    }
 }
